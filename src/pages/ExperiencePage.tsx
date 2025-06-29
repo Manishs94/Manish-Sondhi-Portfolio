332f48374adeca36e6948d614b9d2bff5675360d
Manish Sondhi <58338759+Manishs94@users.noreply.github.com>
2025-06-29 13:48:09 -0400

    Remove Breadcrumb Navigation

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import Experience from '@/components/Experience';
 import Education from '@/components/Education';
 import SkillsVisualization from '@/components/SkillsVisualization';
-import BreadcrumbNav from '@/components/BreadcrumbNav';
 import Footer from '@/components/Footer';
 import ScrollProgress from '@/components/ScrollProgress';
 
@@ -14,9 +12,6 @@ const ExperiencePage = () => {
       <ScrollProgress />
       <Navbar />
       <main className="pt-16">
-        <div className="section-container">
-          <BreadcrumbNav />
-        </div>
         <Experience />
         <SkillsVisualization />
         <Education />
