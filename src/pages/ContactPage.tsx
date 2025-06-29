5426753e9dc4c0be385f793ff49063fb48010768
Manish Sondhi <58338759+Manishs94@users.noreply.github.com>
2025-06-29 13:48:33 -0400

    Remove Contact Page Breadcrumb Navigation

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import Navbar from '@/components/Navbar';
 import Contact from '@/components/Contact';
 import TestimonialsCarousel from '@/components/TestimonialsCarousel';
 import NewsletterSignup from '@/components/NewsletterSignup';
-import BreadcrumbNav from '@/components/BreadcrumbNav';
 import Footer from '@/components/Footer';
 import ScrollProgress from '@/components/ScrollProgress';
 
@@ -14,9 +12,6 @@ const ContactPage = () => {
       <ScrollProgress />
       <Navbar />
       <main className="pt-16">
-        <div className="section-container">
-          <BreadcrumbNav />
-        </div>
         <Contact />
         <TestimonialsCarousel />
         
